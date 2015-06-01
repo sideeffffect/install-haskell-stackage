d4e27582c0308ca966f11a4a8940fe60cfb0bee4
Ondra Pelech <ondra.pelech@gmail.com>
2015-06-02 01:11:07 +0200

    fix curl usage

--- install-haskell-stackage.sh
+++ install-haskell-stackage.sh
@@ -96,13 +96,13 @@ echo -e "\033[1m###  Installing GHC...  ########################################
   
   if [ ! -e $GHC_TAR ]; then
     echo -e "\033[1mDownloading GHC\033[m"
-    curl -L "${GHC_SOURCE}/${GHC_VER}/${GHC_TAR}"
+    curl -O -L "${GHC_SOURCE}/${GHC_VER}/${GHC_TAR}"
   else
     echo -e "\033[1mUsing already downloaded GHC in \033[m${TMPDIR}"
   fi
   
   [ -e SHA256SUMS ] && rm SHA256SUMS
-  curl -L "${GHC_SOURCE}/${GHC_VER}/SHA256SUMS"
+  curl -O -L "${GHC_SOURCE}/${GHC_VER}/SHA256SUMS"
   CHECK=$(sha256sum -c SHA256SUMS 2>&1 | grep "${GHC_TAR}: ")
   if [ "${CHECK}" != "${GHC_TAR}: OK" ]; then
     echo
@@ -146,7 +146,7 @@ echo -e "\033[1m###  Installing cabal-install...  ##############################
   [ -e $CABAL_TAR ] && rm $CABAL_TAR
   
   echo -e "\033[1mDownloading cabal-install\033[m"
-  curl -L ${CABAL_SOURCE}/cabal-install-${CABAL_VER}/${CABAL_TAR}
+  curl -O -L ${CABAL_SOURCE}/cabal-install-${CABAL_VER}/${CABAL_TAR}
   
   echo -e "\033[1mInstalling cabal-install\033[m"
   [ -e cabal-install-${CABAL_VER} ] && rm -rf cabal-install-${CABAL_VER}
